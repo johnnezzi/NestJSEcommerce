d6c0e47e62279dcce3ec81a81bc428738c5e8c39
John Nezzi <johnnezzi@yahoo.com>
2020-04-11 17:01:23 +0100

    create user model

---
+++ src/models/user.schema.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose'
+
+export const Userschema = new mongoose.Schema({
+  name: String,
+  password: String,
+  seller: {
+    type: Boolean,
+    default: false,
+  },
+  address: {
+    addr1: String,
+    addr2: String,
+    city: String,
+    county: String,
+    postcode: String,
+    country: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
