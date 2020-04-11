ae304ac2e72dbfba604571ca8506e20c54466c0b
John Nezzi <johnnezzi@yahoo.com>
2020-04-11 17:23:07 +0100

    create the Product Schema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose'
 
-export const Userschema = new mongoose.Schema({
+export const UserSchema = new mongoose.Schema({
   name: String,
   password: String,
   seller: {
