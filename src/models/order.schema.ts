77aee468bbdc666b5c6b30b1d4c718098d5da6de
John Nezzi <johnnezzi@yahoo.com>
2020-04-12 09:50:04 +0100

    m

--- src/models/order.schema.ts
+++ src/models/order.schema.ts
@@ -1,5 +1,20 @@
 import * as mongoose from 'mongoose'
 
 export const OrderSchema = new mongoose.Schema({
-
-})
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  totalPrice: {
+    type: Number,
+    default: 0
+  },
+  products: [
+    {
+      product : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+      }
+    }
+  ]
+});
